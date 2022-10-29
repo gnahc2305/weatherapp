27745df78b4f02ad4eecd934113d163d4f9199cd
gnahc2305 <achangmurillo@gmail.com>
2022-10-28 23:35:34 -0400

    create function to format data thats only needed

--- src/index.js
+++ src/index.js
@@ -8,16 +8,32 @@ function getWeatherInfo(location) {
   )
     .then(function (response) {
       return response.json();
+      // console.log(response.json());
     })
     .then(function (response) {
-      let currentWeather = response.main.temp;
-      console.log(currentWeather);
+      // return response
+      // let currentWeather = response.main.temp;
+      // return currentWeather;
+      // console.log(response);
+      // console.log(formatData(response));
+      formatData(response)
     })
     .catch(function (err) {
       console.log("Error " + err);
     });
 }
 
-btn.addEventListener('click', () => {
-    getWeatherInfo(input.value)
-})
+btn.addEventListener("click", () => {
+  getWeatherInfo(input.value);
+});
+
+function formatData(data) {
+  // data.slice(data.main.temp);
+  let temp = data.main.temp
+  let feelsLike = data.main.feels_like;
+  let humidity = data.main.humidity;
+
+  let weather = data.weather[0].main;
+  let description = data.weather[0].description;
+  // console.log(temp, feelsLike, humidity, weather, description);
+}
